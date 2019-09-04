d597b6e2357fa2140d48b203e9c15ef5db079e11
hoandreas <andreas_ho@hotmail.com>
2019-09-04 12:36:24 +0700

    submit tugad string to array

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// CARA 1
 function convertStringToArray(str) {
     let splitted = str.split(",");
     let output = [];
@@ -9,6 +10,27 @@ function convertStringToArray(str) {
         }
         output.push(tempOutput);
     }
+    return output;
+}
+
+console.log(convertStringToArray('qwer,tyui,asdf,ghjk'));
+console.log(convertStringToArray('milotic,serperior,dragonair'));
+
+
+// CARA 2
+function convertStringToArray(str) {
+    let output = [];
+    let tempOutput = [];
+
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] == ',') {
+            output.push(tempOutput);
+            tempOutput = [];
+        } else {
+            tempOutput.push(str[i]);
+        }
+    }
+    output.push(tempOutput);
     return output;
 }
 
